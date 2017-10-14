b36a78e177bb3829a04ae4d2148ebb3b55e9e282
Antoine Dozois <dozois.a@gmail.com>
2017-10-13 22:22:30 -0400

    Architecture principale pour AutomatonCppLiteralExtraction

--- XtractL/XtractL/tools/AutomatonCppLiteralExtraction.cpp
+++ XtractL/XtractL/tools/AutomatonCppLiteralExtraction.cpp
@@ -1,7 +1,142 @@
 #include "AutomatonCppLiteralExtraction.h"
 
+#include "State.h"
+#include "MatchAllSymbols.h"
+#include "MatchListSymbols.h"
+#include "MatchRangeSymbols.h"
+#include "MatchSingleSymbol.h"
+#include "MatchNotSingleSymbol.h"
+#include "MatchNotListSymbols.h"
+
 AutomatonCppLiteralExtraction::AutomatonCppLiteralExtraction() {
 }
 
 AutomatonCppLiteralExtraction::~AutomatonCppLiteralExtraction() {
+}
+
+void AutomatonCppLiteralExtraction::createStates(void) {
+	sCode = new State("Code", true);
+	sSlash = new State("Slash", false);
+	sCStyleComment = new State("C style comment", false);
+	sCStyleCommentStar = new State("C style comment star", false);
+	sCppStyleComment = new State("C++ style comment", false);
+	sCppStyleCommentEscapeChar = new State("C++ style comment escape char", false);
+	sString = new State("String", false);
+	sStringEscapeChar = new State("String escape char", false);
+	sCharacter = new State("Character", false);
+	sCharacterEscapeChar = new State("Character escape char", false);
+	//TODO implementer les states pour Interger et Float
+}
+
+void AutomatonCppLiteralExtraction::createTransitions(AutomatonFileStatExtraction  const & obj) {
+	// Code et Slash
+	tSlashEnter = new Transition("Slash enter", new MatchSingleSymbol('/'), *sSlash);
+	tSlashExit = new Transition("Slash exit", new MatchListSymbols({ '*','/' }), *sCode);
+	
+	//C comment style
+	tCStyleCommentEnter = new TransitionCounter("C style comment enter", new MatchSingleSymbol('*'), *sCStyleComment);
+	tCStyleCommentChar = new Transition("C style comment char", new MatchNotSingleSymbol('*'), *sCStyleComment);
+	tCStyleCommentStarEnter = new Transition("C star comment enter", new MatchSingleSymbol('*'), *sCStyleCommentStar);
+	tCStyleCommentStarAgain = new Transition("C star comment again", new MatchSingleSymbol('*'), *sCStyleCommentStar);
+	tCStyleCommentStarExit = new Transition("C star comment exit", new MatchNotListSymbols({ '*','/' }), *sCStyleComment);
+	tCStyleCommentExit = new Transition("C style commnent exit", new MatchSingleSymbol('/'), *sCode);
+	
+	//C++ comment style
+	tCppStyleCommentEnter = new TransitionCounter("Cpp style comment enter", new MatchSingleSymbol('/'), *sCppStyleComment);
+	tCppStyleCommentChar = new Transition("Cpp style comment char", new MatchNotListSymbols({ '\\', '\n' }), *sCppStyleComment);
+	tCppStyleCommentEscapeCharEnter = new Transition("Cpp style comment escape character enter", new MatchSingleSymbol('\\'), *sCppStyleCommentEscapeChar);
+	tCppStyleCommentEscapeCharExit = new Transition("Cpp style comment escape character exit", new MatchAllSymbols(), *sCppStyleComment);
+	tCppStyleCommentExit = new Transition("Cpp style comment character exit", new MatchSingleSymbol('\n'), *sCode);
+	
+	//String 
+	tStringEnter = new TransitionCounter("String enter", new MatchSingleSymbol('\"'), *sString);
+	tStringEscapeCharEnter = new Transition("String escape character", new MatchSingleSymbol('\\'), *sStringEscapeChar);
+	tStringEscapeCharExit = new Transition("String escape character", new MatchAllSymbols(), *sString);
+	tStringExit = new Transition("String exist", new MatchSingleSymbol('\"'), *sCode);
+
+	//Char
+	tCharacterEnter = new TransitionCounter("Char enter", new MatchSingleSymbol('\''), *sCharacter);
+	tCharacterEscapeCharEnter = new Transition("Char escape char enter", new MatchSingleSymbol('\\'), *sCharacterEscapeChar);
+	tCharacterEscapeCharExit = new Transition("Char escape char exit", new MatchAllSymbols(), *sCharacter);
+	tCharacterExit = new Transition("Char exit", new MatchListSymbols({ '\'','\n' }), *sCode);
+
+	//Interger
+	//TODO implementer les transitions
+
+	//Float
+	//TODO implementer les transitions
+}
+
+void AutomatonCppLiteralExtraction::assignTransitions(void) {
+	sCode->addTransition(tSlashEnter);
+	sCode->addTransition(tStringEnter);
+	sCode->addTransition(tCharacterEnter);
+
+	sSlash->addTransition(tCStyleCommentEnter);
+	sSlash->addTransition(tCppStyleCommentEnter);
+	sSlash->addTransition(tSlashExit);
+
+	sCStyleComment->addTransition(tCStyleCommentChar);
+	sCStyleComment->addTransition(tCStyleCommentStarEnter);
+
+	sCStyleCommentStar->addTransition(tCStyleCommentExit);
+	sCStyleCommentStar->addTransition(tCStyleCommentStarAgain);
+	sCStyleCommentStar->addTransition(tCStyleCommentStarExit);
+
+	sCppStyleComment->addTransition(tCppStyleCommentChar);
+	sCppStyleComment->addTransition(tCppStyleCommentEscapeCharEnter);
+	sCppStyleComment->addTransition(tCppStyleCommentExit);
+
+	sCppStyleCommentEscapeChar->addTransition(tCppStyleCommentEscapeCharExit);
+
+	sString->addTransition(tStringEscapeCharEnter);
+	sString->addTransition(tStringExit);
+
+	sStringEscapeChar->addTransition(tStringEscapeCharExit);
+
+	sCharacter->addTransition(tCharacterEscapeCharEnter);
+	sCharacter->addTransition(tCharacterExit);
+
+	sCharacterEscapeChar->addTransition(tCharacterEscapeCharExit);
+
+	//Interger
+	//TODO addTransition interger
+
+	//Float
+	//TODO addTransition float
+}
+
+void AutomatonCppLiteralExtraction::addStates(void) {
+	addState(sCode);
+	addState(sSlash);
+	addState(sCStyleComment);
+	addState(sCStyleCommentStar);
+	addState(sCppStyleComment);
+	addState(sCppStyleCommentEscapeChar);
+	addState(sString);
+	addState(sStringEscapeChar);
+	addState(sCharacter);
+	addState(sCharacterEscapeChar);
+	//TODO Pour interger et float
+}
+
+void AutomatonCppLiteralExtraction::assignInitialState(void) {
+	setInitialState(sCode);
+}
+
+uint32_t AutomatonCppLiteralExtraction::cStyleCommentCount(void) const {
+	//return sCStyleComment->transitionCount();
+	return tCStyleCommentEnter->count();
+}
+
+uint32_t AutomatonCppLiteralExtraction::cppStyleCommentCount(void) const {
+	return tCppStyleCommentEnter->count();
+}
+
+uint32_t AutomatonCppLiteralExtraction::stringCount(void) const {
+	return tStringEnter->count();
+}
+
+uint32_t AutomatonCppLiteralExtraction::characterCount(void) const {
+	return tCharacterEnter->count();
 }
--- XtractL/XtractL/tools/AutomatonCppLiteralExtraction.h
+++ XtractL/XtractL/tools/AutomatonCppLiteralExtraction.h
@@ -6,6 +6,8 @@
 //!
 //! \version 1.0
 //!
+//! \author		Tony Wong, Ph.D.
+//! \author		JC Demers
 //! \author Antoine Dozois
 //! \author	Guillaume Chabot-Norbet
 //!
@@ -15,11 +17,126 @@
 #ifndef LITERAL_EXTRACTION_H
 #define LITERAL_EXTRACTION_H
 
-class AutomatonCppLiteralExtraction
+#include "FiniteStateTransducer.h"
+
+class AutomatonFileStatExtraction;
+
+//! \brief		Automate dédié à l'extraction des literaux d'
+//!				un fichier du langage C/C++.
+//! \details	Cette classe fait partie de la solution XtractC. 
+//!
+//! Cette classe est grandement basé et inspiré de Automaton
+//! CppCommentExtraction
+//! 
+//! L'automate est complet et autonome.
+//! 
+//! Plusieurs fonctions permettent de retourner quelques données 
+//! statistiques :
+//!  - le nombre de d'entier
+//!  - le nombre de point flottant
+//!  - le nombre de commentaire style C
+//!  - le nombre de commentaire style C++
+//!  - le nombre de chaînes de caractères (`"abc"`)
+//!  - le nombre de caractères (`'a'`)
+//! 
+//! \author		Tony Wong, Ph.D.
+//! \author		JC Demers
+//! \author		Antoine Dozois
+//! \author		Guillaume Chabot-Nobert
+//! \version	1.0
+//! \date		2017
+//! \copyright	Copyleft
+
+class AutomatonCppLiteralExtraction final: public FiniteStateTransducer
 {
 public:
 	AutomatonCppLiteralExtraction();
 	~AutomatonCppLiteralExtraction();
+
+	//! Retourne le nombre de commentaire style-C : 
+	//! `/* commentaire */`
+	uint32_t cStyleCommentCount(void) const;
+	//! Retourne le nombre de commentaire style-C++ : 
+	//! `// commentaire`
+	uint32_t cppStyleCommentCount(void) const;
+	//! Retourne le nombre de chaînes de caractères tel 
+	//! que défini en C/C++ : `"abc"`
+	uint32_t stringCount(void) const;
+	//! Retourne le nombre de caractères tel que 
+	//! défini en C/C++ : `'a'`
+	uint32_t characterCount(void) const;
+
+	//TODO fonction de compteur pour la classe interger et float
+
+
+private:
+
+	// States reliés à l'état Code
+	State * sCode;
+
+	// States reliés à l'état Slash
+	State * sSlash;
+
+
+	// States reliés à l'état CStyleComment
+	State * sCStyleComment;
+	State * sCStyleCommentStar;
+
+	// States reliés à l'état CppStyleComment
+	State * sCppStyleComment;
+	State * sCppStyleCommentEscapeChar;
+
+	// States reliés à l'état String
+	State * sString;
+	State * sStringEscapeChar;
+
+	// States reliés à l'état Char
+	State * sCharacter;
+	State * sCharacterEscapeChar;
+
+	// States reliés à l'état Entier
+	//TODO ajouter les etats pour les entiers
+
+	// States reliés à l'état Point Flottant\
+	//TODO ajouter les etats pour les flottants
+
+	// Transition de Code
+	Transition * tSlashEnter;
+	Transition * tSlashExit;
+
+	TransitionCounter * tCStyleCommentEnter;
+	Transition * tCStyleCommentChar;
+	Transition * tCStyleCommentStarEnter;
+	Transition * tCStyleCommentStarExit;
+	Transition * tCStyleCommentStarAgain;
+	Transition * tCStyleCommentExit;
+
+	TransitionCounter * tCppStyleCommentEnter;
+	Transition * tCppStyleCommentChar;
+	Transition * tCppStyleCommentEscapeCharEnter;
+	Transition * tCppStyleCommentEscapeCharExit;
+	Transition * tCppStyleCommentExit;
+
+	TransitionCounter * tStringEnter;
+	//Transition * tStringChar;
+	Transition * tStringEscapeCharEnter;
+	Transition * tStringEscapeCharExit;
+	Transition * tStringExit;
+
+	TransitionCounter * tCharacterEnter;
+	//Transition * tCharacterChar;
+	Transition * tCharacterEscapeCharEnter;
+	Transition * tCharacterEscapeCharExit;
+	Transition * tCharacterExit;
+
+
+	// Private Method
+	void createStates(void);
+	void createTransitions(AutomatonFileStatExtraction const & automatonFileStatExtraction);
+	void assignTransitions(void);
+	void addStates(void);
+	void assignInitialState(void);
+
 };
 
 #endif //! LITERAL_EXCEPTION_H
